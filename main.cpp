403c343a38d663ed9f045610027699c277b27116
Титов Дима <dima.titov18@gmail.com>
2020-04-15 18:40:43 +0300

    ж

--- main.cpp
+++ main.cpp
@@ -36,23 +36,25 @@ int main(int argc, char* argv[])
 
     //читаем массив
     ifstream fRead("Text.txt", ios_base::binary);
-    
-    if (fRead.is_open()) {
+    int n = 10;
+    char* buffer = new char[11][11];
+    if (fRead) {
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++) {
-                fRead >> mass2[i][j];
+                fRead.read(buffer, n);
             }
     }
     else
         cout << "File cannot be open";
     fRead.close();
 
-
-    // выводим на экран mass2
-    for(int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            cout << mass2[i][j];
     
+    // выводим на экран mass2
+    /*for(int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            cout << mass2[i][j];*/
+    for (int i = 0; i < 10; i++)
+        cout << buffer[i];
     
 
 
